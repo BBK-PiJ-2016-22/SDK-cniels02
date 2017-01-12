a998f28c3308bfb1900382c5ea525a32960e2780
BBK-PiJ-2016-22 <caspertellerup@gmail.com>
2017-01-12 11:44:43 +0000

    first commit

---
+++ Day1/src/exercise3/NumberPersonalities.scala
@@ -0,0 +1,48 @@
+package exercise3
+
+/**
+  * Created by Casper on 12/01/2017.
+  */
+object NumberPersonalities {
+  val limit = 100
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def isPrime(n: Int): Boolean ={
+  true
+  }
+
+  def isHappy(n: Int): Boolean ={
+    true
+  }
+  def isTriangular(n: Int): Boolean={
+    true
+  }
+  def isSquare(n: Int): Boolean ={
+    true
+  }
+  def isSmug(n: Int): Boolean = {
+    true
+  }
+  def isHonest(n: Int): Boolean ={
+    true
+  }
+  def isPronic(n: Int): Boolean ={
+    true
+  }
+  def isDeficient(n: Int): Boolean ={
+    true
+  }
+  def isPerfect(n: Int): Boolean ={
+    true
+  }
+  def isAbundant(n: Int): Boolean={
+    true
+  }
+
+  def sumOfPositiveDivisorsOf(n: Int): Int ={
+    9
+  }
+}
