a27e0fcd733c6c367d092f37f2f7437aae66e5b3
BBK-PiJ-2016-22 <caspertellerup@gmail.com>
2017-02-13 16:16:03 +0000

    Day 5 added

---
+++ Day5/src/Adder.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by Casper on 13/02/2017.
+  */
+
+
+class Adder(amount: Int) {
+  def add(in: Int) = in + amount
+}
+
+
--- Day5/src/Counter.scala
+++ Day5/src/Counter.scala
@@ -1,6 +1,29 @@
 /**
   * Created by Casper on 13/02/2017.
   */
-class Counter {
 
+object Counter extends App{
+  val v = new Counter(10)
+  print(v.dec().inc().inc().count)
 }
+
+
+case class Counter(input: Int = 0) {
+
+  def inc(a: Int = 1): Counter ={
+    this.copy(input + 1)
+  }
+
+  def dec(a: Int = 1): Counter = {
+    this.copy(input -1)
+  }
+
+  def count:Int = input
+
+  def adjust(a: Adder): Counter ={
+    new Counter(a.add(input))
+  }
+}
+
+
+
