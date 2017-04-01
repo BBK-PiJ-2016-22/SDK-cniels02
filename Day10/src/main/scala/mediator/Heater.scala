912e4bf70cdcbf38b9c49fb02072f05d10468257
BBK-PiJ-2016-22 <caspertellerup@gmail.com>
2017-04-01 23:26:56 +0100

    Day 10 added

--- Day10/src/main/scala/mediator/Heater.scala
+++ Day10/src/main/scala/mediator/Heater.scala
@@ -2,9 +2,25 @@ package mediator
 
 class Heater extends Colleague {
 
-  override def setMediator(mediator: MachineMediator): Unit = ???
+  private var mediator: MachineMediator = _
 
-  def on(temp: Int): Unit = ???
+  override def setMediator(mediator: MachineMediator): Unit = this.mediator = mediator
 
-  def off(): Unit = ???
+  def on(temp: Int): Unit = {
+    println("Heater is on...")
+
+    if(mediator.checkTemperature(temp)){
+      println(s"Temperature reached $temp")
+      println(s"Temperature is set to $temp")
+      mediator.off()
+    }
+  }
+
+  def off(): Unit = {
+    println("Heater is off...")
+    mediator.wash()
+  }
+
+
+
 }
